92e90830e88402c55abde93b90c43cc8475431e3
yogeshpatel09 <yogeshpatel7028@gmail.com>
2025-09-21 12:00:09 +0530

    Enhance Header component interactivity by adding cursor styles to clickable elements for improved user experience

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -13,9 +13,9 @@ function Header({ onToggleLeft, onToggleRight, pageTitle }) {
             <div className="h-16 border-b  w-full border-[rgba(28,28,28,0.1)] dark:border-[rgba(255,255,255,0.1)] px-7 py-5 flex justify-between">
                 <div className="flex w-full dark:text-white">
 
-                    <div onClick={() => onToggleLeft && onToggleLeft()} className='px-2 py-1'><MdOutlineViewSidebar /></div>
-                    <div className='px-2 py-1'><FaRegStar /></div>
-                    <div className="flex">
+                    <div onClick={() => onToggleLeft && onToggleLeft()} className='px-2 py-1 cursor-pointer'><MdOutlineViewSidebar /></div>
+                    <div className='px-2 py-1 cursor-pointer'><FaRegStar /></div>
+                    <div className="flex cursor-pointer">
                         <Breadcrumb />
                     </div>
 
@@ -30,20 +30,20 @@ function Header({ onToggleLeft, onToggleRight, pageTitle }) {
                             placeholder='Search'
                             className='dark:bg-[rgba(255,255,255,0.1)] bg-[rgba(28,28,28,0.05)] rounded-full pl-7 h-7 text-xs py-0.5  w-[160px] outline-none'
                         />
-                        <div className="relative -top-7  -right-32 dark:text-[rgba(255, 255, 255, 0.2)] w-7">
+                        <div className="relative -top-7 cursor-pointer -right-32 dark:text-[rgba(255, 255, 255, 0.2)] w-7">
                             ⌘/
                         </div>
                     </div>
-                    <div className='px-2 py-1'>
+                    <div className='px-2 py-1 cursor-pointer'>
                         <ThemeToggle />
                     </div>
-                    <div className='px-2 py-1 hidden xl:block'>
+                    <div className='px-2 py-1 hidden xl:block cursor-pointer'>
                         <FaClockRotateLeft />
                     </div>
-                    <div className='px-2 py-1 hidden xl:block'>
+                    <div className='px-2 py-1 hidden xl:block cursor-pointer'>
                         <VscBell />
                     </div>
-                    <div onClick={() => onToggleRight && onToggleRight()} className='px-2 py-1 hidden lg:block'>
+                    <div onClick={() => onToggleRight && onToggleRight()} className='px-2 py-1 hidden lg:block cursor-pointer'>
                         <MdOutlineViewSidebar />
                     </div>
                 </div>
